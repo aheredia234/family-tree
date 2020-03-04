840511895cca906c4e7260f7ef37b807f97ce9eb
Angie <42586731+aheredia234@users.noreply.github.com>
2020-03-04 12:20:34 -0600

    Add files via upload

---
+++ Family Tree/Family Tree/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Family Tree
+//
+//  Created by Angela Heredia on 3/2/20.
+//  Copyright © 2020 Angela Heredia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
